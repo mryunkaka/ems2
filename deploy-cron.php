15e8ba526e9b087ea01fdf8ed5d0f722be6ea7d6
Sayid Adam <mryunkaka@gmail.com>
2026-05-01 20:11:14 +0800

    tes push5

--- deploy-cron.php
+++ deploy-cron.php
@@ -6,15 +6,31 @@ $repo = '/home/hark8423/public_html/rh-ems2';
 $log = '/home/hark8423/git-deploy.log';
 $git = is_executable('/usr/bin/git') ? '/usr/bin/git' : 'git';
 
+function respond(string $message, string $logFile, bool $writeLog = true, int $exitCode = 0): void
+{
+    $date = date('Y-m-d H:i:s');
+
+    if ($writeLog) {
+        file_put_contents($logFile, $date . ' - ' . $message . PHP_EOL, FILE_APPEND);
+    }
+
+    if (PHP_SAPI !== 'cli') {
+        header('Content-Type: text/plain; charset=UTF-8');
+        echo $message;
+    }
+
+    exit($exitCode);
+}
+
 if (!is_dir($repo . '/.git')) {
-    exit(1);
+    respond('ERROR: folder .git tidak ditemukan di ' . $repo, $log, true, 1);
 }
 
 chdir($repo);
 
 $old = trim((string) shell_exec($git . ' rev-parse HEAD 2>&1'));
 if ($old === '' || preg_match('/fatal:|not found|unable to/i', $old)) {
-    exit(1);
+    respond('ERROR: gagal membaca HEAD. output=' . $old, $log, true, 1);
 }
 
 $pullOutput = trim((string) shell_exec($git . ' pull origin main 2>&1'));
@@ -22,10 +38,13 @@ $new = trim((string) shell_exec($git . ' rev-parse HEAD 2>&1'));
 
 if (
     $new === '' ||
-    preg_match('/fatal:|not found|unable to/i', $new) ||
-    $old === $new
+    preg_match('/fatal:|not found|unable to/i', $new)
 ) {
-    exit(0);
+    respond('ERROR: git pull / rev-parse gagal. pull=' . $pullOutput . ' | head=' . $new, $log, true, 1);
+}
+
+if ($old === $new) {
+    respond('OK: already up to date', $log, false, 0);
 }
 
 $commits = trim((string) shell_exec(
@@ -38,7 +57,7 @@ if ($commits === '' || preg_match('/fatal:|not found|unable to/i', $commits)) {
     ));
 
     if ($singleCommit === '' || preg_match('/fatal:|not found|unable to/i', $singleCommit)) {
-        exit(0);
+        respond('OK: deploy berhasil, tapi detail commit tidak terbaca', $log, false, 0);
     }
 
     $commits = $singleCommit;
@@ -54,3 +73,5 @@ foreach (preg_split('/\r\n|\r|\n/', $commits) as $commit) {
 
     file_put_contents($log, $date . ' - Deploy ' . $commit . PHP_EOL, FILE_APPEND);
 }
+
+respond('OK: deploy selesai', $log, false, 0);
